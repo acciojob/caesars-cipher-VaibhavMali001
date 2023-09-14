dc6bf55e9f1f59f5c8005b9814c1def20559dabf
Vaibhav Rameshwar Mali <malivaibhav.7620@gmail.com>
2023-09-14 13:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,15 +32,24 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  let decodedArr = [];
 
-  return; //return decodedArr
+  for (let i = 0; i < encodedStr.length; i++) {
+    const char = encodedStr[i];
+    if (lookup[char] !== undefined) {
+      // Check if the character is in the lookup table
+      decodedArr.push(lookup[char]);
+    } else {
+      // If not in the table, push the character unchanged
+      decodedArr.push(char);
+    }
+  }
+
+  return decodedArr.join(''); // Join the characters into a string and return it
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+// Test the function
+console.log(rot13("SERR YBIR? NPPVBWBO")); 
 
 // Do not change this line
 window.rot13 = rot13;
